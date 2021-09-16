d6704934341f8b7357eef2f04bc8b8a49db163d8
Krysles <gls.christophe@icloud.com>
2021-09-16 11:37:40 +0200

    Mise en place d'une route de base /api pour tester le routeur

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const package = require('./package.json');
 
 const port = process.env.PORT || 5000;
+const apiRoot = '/api';
 
 const app = express();
 
@@ -12,6 +14,15 @@ app.use(cors({
     origin: /http:\/\/localhost/
 }));
 app.options('*', cors());
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send(`${package.name} - v${package.version}`);
+});
+
+app.use(apiRoot, router);
+
 app.listen(port, () => {
     console.log('Server is up!');
 });
